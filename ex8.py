c54f48b75b015e6b2bb200d81047cc1442ea3e2c
Ruben-hash <58661892+Ruben-hash@users.noreply.github.com>
2020-12-10 11:50:56 +0100

    Add files via upload

--- ex8.py
+++ ex8.py
@@ -2,13 +2,12 @@
 
 #2
 def bL(binary):
-    binary1 = binary
     decimal, i  = 0, 0
     while(binary != 0):
         dec = binary % 10
         decimal = decimal + dec * 2**i
         binary = binary//10
         i += 1
-    print('Decimal equivalent of {} is {}'.format(binary1, decimal))
+    print('sa forme binaire est {}'.format(decimal))
 
 
